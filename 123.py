3b4acdc2397cbef90cb6771ffea584550859484d
itcube <misha.dorenskiy@mail.ru>
2022-02-03 18:01:09 +1000

    Показ карты

--- 123.py
+++ 123.py
@@ -1,8 +1,10 @@
 import sys
+import requests
+from os import remove
 
 # Импортируем из PyQt5.QtWidgets классы для создания приложения и виджета
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
-from PyQt5.QtWidgets import QLabel, QLineEdit
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtGui import QPixmap
 
 
 # Унаследуем наш класс от простейшего графического примитива QWidget
@@ -11,9 +13,41 @@ class Example(QWidget):
         super().__init__()
         self.initUI()
 
+    def getImage(self):
+        lon = 37.530887
+        lat = 55.703118
+        delta = 0.002
+
+        paramsz = {'ll': ','.join([str(lon), str(lat)]), 'spn': ','.join([str(delta), str(delta)]), 'l': 'map'}
+
+        map_request = "http://static-maps.yandex.ru/1.x/"
+        response = requests.get(map_request, params=paramsz)
+
+        if not response:
+            print("Ошибка выполнения запроса:")
+            print(map_request)
+            print("Http статус:", response.status_code, "(", response.reason, ")")
+            sys.exit(1)
+
+        # Запишем полученное изображение в файл.
+        self.map_file = "map.png"
+        with open(self.map_file, "wb") as file:
+            file.write(response.content)
+
     def initUI(self):
-        self.setFixedSize(450, 450)
+        self.setFixedSize(600, 600)
         self.setWindowTitle('Яндекс карты')
+
+        self.getImage()
+
+        self.pixmap = QPixmap(self.map_file)
+        self.image = QLabel(self)
+        self.image.move(0, 0)
+        self.image.resize(600, 600)
+        self.image.setPixmap(self.pixmap)
+
+        remove(self.map_file)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
